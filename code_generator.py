47c8f96a9270ae1cbf55d8cf61b9d9beff667e99
yzwang <yzwang@outlook.com>
2017-08-08 15:43:02 -0700

    1. Add "concolic' attribute to config file 2. Improve code generation based on e1000_ioctl.c 3. Add crete support

--- code_generator.py
+++ code_generator.py
@@ -6,79 +6,136 @@ import sys
 import os
 import datetime
 
+_platform = "i386"
 
-def create_stack_posthandler(params):
+def get_size(type_name=str):
+    "Return the size of given type"
+    if "*" in type_name and "i386" == _platform:
+        return 4
+    elif "char" in type_name:
+        return 1
+    elif "long" in type_name and _platform == "i386":
+        return 4
+    elif "int" in type_name:
+        return 4
+    elif "32" in type_name:
+        return 4
+    else:
+        return 4
+
+def create_stack_entry_handler(params):
     "Used to generate post handler function"
-    _params = "static void handler_post (struct kprobe *p, struct pt_regs *regs,\n unsigned long flags)\n"
-    _stack_size = 8
-    _code = "{\n"
+    _code = "static int entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)\n"
+    _code += "{\n"
+    _code += "crete_capture_begin();\n"
     _code += "char *sp_regs = kernel_stack_pointer (regs);\n"
-    _code += "char *params = NULL;\n"
+
+    _stack_size = 4
     for i in range(len(params)):
-        _code += "params = sp_regs + %d;\n" % (_stack_size)
-        _code += "pr_info(\"<%s> param{} addr: 0x%p, value:0x%lx\\n\",\n\
-        p->symbol_name, params, *(unsigned int *)params);\n".format (i + 1)
+        if params[i]["attribute"] == True:
+            _code += "char* arg{}_addr = sp_regs + {};\n".format(i + 1, _stack_size)
+            _code += "size_t arg{}_size = {};\n".format(i + 1, get_size(params[i]["type"]))
+            _code += "crete_make_concolic(arg{}_addr, arg{}_size, \"crete_probe_arg{}\");\n".format(i + 1, i + 1, i + 1)
         _stack_size += 4
-    _code += "\n}\n\n"
-    return _params + _code
+    
+    _code += "return 0;\n"
+    _code += "}\n"
+    return _code
 
 def create_register_posthandler(params):
     "Used to generate post handler function"
-    _params = "static void handler_post (struct kprobe *p, struct pt_regs *regs,\n unsigned long flags)\n"
-    _stack_size = 4
-    _code = "{\n"
+    _code = "static int entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)\n"
+    _code += "{\n"
+    _code += "crete_capture_begin();\n"
     _code += "char *sp_regs = kernel_stack_pointer (regs);\n"
-    _code += "char *params = NULL;\n"
+
+    _stack_size = 4
     for i in range(len(params)):
-        if i == 0:
-            _code += "pr_info(\"<%s> param{} value:0x%lx\\n\",\n\
-            p->symbol_name, regs->ax);\n".format(i + 1)
-        elif i == 1:
-            _code += "pr_info(\"<%s> param{} value:0x%lx\\n\",\n\
-            p->symbol_name, regs->dx);\n".format(i + 1)
-        elif i == 2:
-            _code += "pr_info(\"<%s> param{} value:0x%lx\\n\",\n\
-            p->symbol_name, regs->cx);\n".format(i + 1)
-        else:
-            _code += "params = sp_regs + %d;\n" % (_stack_size)
-            _code += "pr_info(\"<%s> esp addr: 0x%p, value:0x%lx\\n\",\n\
-            p->symbol_name, params, *(unsigned int *)params);\n"
+        if params[i]["attribute"] == True:
+            if i == 0:
+                _code += "crete_make_concolic (&regs->ax, sizeof (regs->ax), \"crete_probe_ax\");\n"
+            elif i == 1:
+                _code += "crete_make_concolic (&regs->dx, sizeof (regs->dx), \"crete_probe_dx\");\n"
+            elif i == 2:
+                _code += "crete_make_concolic (&regs->cx, sizeof (regs->cx), \"crete_probe_cx\");\n"
+            else:
+                _code += "char arg{}_addr = sp_regs + {};\n".format(i + 1, _stack_size)
+                _code += "size_t arg{}_size = {};\n".format(i + 1, get_size(params[i]["type"]))
+                _code += "crete_make_concolic(arg{}_addr, arg{}_size, \"crete_probe_arg{}\");\n".format(i + 1, i + 1, i + 1)
+        if i > 2:
             _stack_size += 4
-    _code += "\n}\n\n"
-    return _params + _code
+
+    _code += "return 0;\n"
+    _code += "}\n"
+    return _code
 
 def code_generator(data):
-    "#Used to generate module code"
-    _head_file = "#include <linux/kernel.h>\n#include <linux/module.h>\n#include <linux/kprobes.h>\n\n"
+    "Used to generate module code"
+    _head_file = "#include <linux/kernel.h>\n"
+    _head_file += "#include <linux/module.h>\n"
+    _head_file += "#include <linux/kprobes.h>\n"
+    _head_file += ("\n")
+
     _marco = "#define MAX_SYMBOL_LEN 64\n"
-    _symbol_string = "static char symbol[MAX_SYMBOL_LEN] = \"{}\";\nmodule_param_string(symbol, symbol,\
-    sizeof(symbol),0644);\n\n".format(data["function_name"])
-    _struct = "static struct kprobe kp = {\n.symbol_name = symbol,\n};\n\n"
+
+    _symbol_string = "static char symbol[MAX_SYMBOL_LEN] = \"{}\";\n".format (data["function_name"])
+    _symbol_string += "module_param_string(symbol, symbol, sizeof(symbol), 0644);\n"
+
+    _crete_announce = "static void (*crete_capture_begin)(void);\n"
+    _crete_announce += "static void (*crete_capture_end)(void);\n"
+    _crete_announce += "static void (*crete_make_concolic)(void*, size_t, const char *);\n"
+
+    _probe_struct = ""
+    _probe_struct += "static struct kretprobe kretp = {\n"
+    _probe_struct += ".handler = ret_handler,\n"
+    _probe_struct += ".entry_handler = entry_handler,\n"
+    _probe_struct += "};\n"
+
     if "ioctl" in data["function_name"]:
-        _post = create_register_posthandler(data["params"])
+        _entry_handler = create_register_posthandler(data["params"])
     else:
-        _post = create_stack_posthandler(data["params"])
-    _main = "static int __init kprobe_init(void)\n\
-    {\n\
-    int ret;\n\
-    kp.post_handler = handler_post;\n\
-    ret = register_kprobe(&kp);\n\
-    if (ret < 0) {\n\
-        pr_err(\"register_kprobe failed, returned %lx\\n\", ret);\n\
-        return ret;\n\
-    }\n\
-    pr_info(\"Planted kprobe at %p\\n\", kp.addr);\n\
-    return 0;\n\
-    }\n\n"
-    _exit = "static void __exit kprobe_exit(void)\n\
-    {\n\
-    unregister_kprobe(&kp);\n\
-    pr_info(\"kprobe at %p unregistered\\n\", kp.addr);\n\
-    }\n\n\n"
-    _module = "module_init(kprobe_init)\n\
-    module_exit(kprobe_exit)\n\
-    MODULE_LICENSE(\"GPL\");\n"
-    return _head_file + _marco + _symbol_string + _struct + _post + _main + _exit + _module
+        _entry_handler = create_stack_entry_handler(data["params"])
+
+    _ret_handler = "static int ret_handler (struct kretprobe_instance *ri, struct pt_regs *regs)\n"
+    _ret_handler += "{\n"
+    _ret_handler += "crete_capture_end();\n"
+    _ret_handler += "return 0;\n"
+    _ret_handler += "}\n"
+
+
+    _main = "static int __init kprobe_init(void)\n"
+    _main += "{\n"
+    _main += "int32_t ret = 0;\n"
+    _main += "kretp.kp.symbol_name = symbol;\n"
+    _main += "ret = register_kretprobe (&kretp);\n"
+    _main += "if (ret < 0) {\n"
+    _main += "printk(KERN_INFO \"register_kretprobe failed, returned %d\\n\", ret);\n"
+    _main += "return -1;\n"
+    _main += "}\n"
+    _main += "printk(KERN_INFO \"Planted return probe at %s :%p\\n\", kretp.kp.symbol_name, kretp.kp.addr);\n"
+    _main += "crete_capture_begin = kallsyms_lookup_name (\"crete_capture_begin\");\n"
+    _main += "crete_capture_end = kallsyms_lookup_name (\"crete_capture_end\");\n"
+    _main += "crete_make_concolic = kallsyms_lookup_name (\"crete_make_concolic\");\n"
+    _main += "if (! (crete_capture_begin && crete_capture_end && crete_make_concolic)) {\n"
+    _main += " printk(KERN_INFO \"[crete] not all function found, please check crete-intrinsics.ko\\n\");\n"
+    _main += "return -1;\n"
+    _main += "}\n"
+    _main += "return 0;\n"
+    _main += "}\n"
+
+    _exit = "static void __exit kprobe_exit(void)\n"
+    _exit += "{\n"
+    _exit += "unregister_kretprobe(&kretp);\n"
+    _exit += "pr_info(\"kretprobe at %p unregistered\\n\", kretp.kp.addr);\n"
+    _exit += "}\n\n\n"
+
+    _module = "module_init(kprobe_init)\n"
+    _module += "module_exit(kprobe_exit)\n"
+    _module += "MODULE_LICENSE(\"GPL\");\n"
+
+    return _head_file + _marco + _symbol_string + _crete_announce \
+    + _entry_handler + _ret_handler + _probe_struct \
+    + _main + _exit + _module
 
 def generate_makefile():
     "To generate makefile"
@@ -112,8 +169,8 @@ def main():
         configure_file = sys.argv[1]
     with open("{}".format(configure_file), "r") as f:
         config_data = json.load(f)
+    
     make_str = ""
-
     for i in config_data:
         code_str = str(code_generator(i))
         with open("./{}/{}.c".format(current_time, i["function_name"]), "w") as f:
